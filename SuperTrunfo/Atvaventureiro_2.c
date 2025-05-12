16152dff81e59cb1e9a641777677242a75ea4c6f
DJSCodes <diegooosantos@hotmail.com>
2025-05-12 18:12:58 +0000

    Treinamento (&&,||, !)

---
+++ SuperTrunfo/Atvaventureiro_2.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+
+int main(){
+
+    int a = -10;
+
+    if (!(a > 0)) { //(!(a> 0)
+        printf("A variável é negativa.\n");
+        //printf("a não é um número positivo\n");
+    } else {
+
+        printf("A variável é positiva.\n");
+    }
+
+
+
+
+
+}
